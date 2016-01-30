af5003722ba0f9122c07b494ccfb02838eb9ec55
Jovian Chen <wolvtodd@gmail.com>
2016-01-30 22:06:35 +0900

    Added PlayerController and DebugScene.

---
+++ Dewitt2/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private PlayerMotor m_motor;
+
+	void Start ()
+	{
+		m_motor = gameObject.AddComponent<PlayerMotor>() as PlayerMotor;
+	}
+	
+	void Update ()
+	{
+		UpdateMovement();
+		UpdateJumpInput();
+    }
+
+	void UpdateMovement()
+	{
+		Vector3 moveVector = getMovementInput();
+		if (m_motor)
+			m_motor.UpdateMovement(ref moveVector);
+	}
+
+	Vector3 getMovementInput()
+	{
+		var deadZone = 0.1f;
+		Vector3 moveVector = Vector3.zero;
+		moveVector.x = Input.GetAxis("Horizontal");
+
+		if (Mathf.Abs(moveVector.x) > deadZone)
+			return moveVector;
+
+		return Vector3.zero;
+	}
+
+	void UpdateJumpInput()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (m_motor)
+				m_motor.Jump();
+		}
+	}
+}
---
+++ Dewitt2/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour
+{
+	private CharacterController m_characterController;
+	
+	private float m_walkSpeed;
+	private float m_verticalSpeed;
+	private float m_jumpSpeed;
+	private float m_gravity;
+
+	private void Start ()
+	{
+		m_characterController = gameObject.AddComponent<CharacterController>() as CharacterController;
+
+		m_walkSpeed = 14.0f;
+		m_gravity	= 98.0f;
+		m_jumpSpeed = 28.0f;
+    }
+
+	public void UpdateMovement(ref Vector3 moveVector)
+	{
+		if (moveVector.magnitude > 1)
+			moveVector.Normalize();
+
+		moveVector *= m_walkSpeed;
+		ApplyGravity(ref moveVector);
+		if (m_characterController)
+			m_characterController.Move(moveVector * Time.deltaTime);
+    }
+
+	public void Jump()
+	{
+		m_verticalSpeed = m_jumpSpeed;
+	}
+
+	private void ApplyGravity(ref Vector3 moveVector)
+	{
+		m_verticalSpeed -= m_gravity * Time.deltaTime;
+		moveVector.y = m_verticalSpeed;
+
+		if (m_characterController && m_characterController.isGrounded)
+		{
+			if (m_verticalSpeed < -1.0f)
+				m_verticalSpeed = -1.0f;
+		}
+    }
+}
