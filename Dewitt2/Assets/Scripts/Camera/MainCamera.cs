6ea62a03a4f5a24ff283c84b279adbfd7688bde7
Jovian Chen <wolvtodd@gmail.com>
2016-02-14 21:54:17 +0900

    Added a simple Camera that traces player based on its local position and euler angle.

---
+++ Dewitt2/Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour
+{
+	private Transform m_targetLookAt;
+	private Vector3 m_cameraRelativePosition; // relative to targetLookAt
+
+	void Start()
+	{
+		GameObject targetLookAtObj = GameObject.Find("TargetLookAt");
+		if (targetLookAtObj != null)
+			m_targetLookAt = targetLookAtObj.transform;
+
+		SetCameraRelativePosition(new Vector3(-15.0f, 5.0f, -15.0f));
+    }
+
+	void LateUpdate()
+	{
+		if (m_targetLookAt)
+		{
+			transform.position =	m_targetLookAt.position +
+									m_targetLookAt.TransformDirection(m_cameraRelativePosition);
+			transform.LookAt(m_targetLookAt);
+		}
+	}
+
+	public void SetCameraRelativePosition(Vector3 position)
+	{
+		m_cameraRelativePosition = position;
+	}
+}
--- Dewitt2/Assets/Scripts/Camera/TargetLookAt.cs
+++ Dewitt2/Assets/Scripts/Camera/TargetLookAt.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class TargetLookAt : MonoBehaviour
 {
+	//
 	private Transform	m_targetTransform;
 	private Player		m_targetPlayer;
 
+	//
 	private float		m_distanceToTarget;
 
-	void Start ()
+	// smooth related
+	private Vector3		m_currentVelocity;
+	private float		m_smoothTime;
+
+	//
+	private Vector3		m_directionPosition;
+
+	void Start()
 	{
-		m_distanceToTarget = 5.0f;
+		SetDistance(2.5f);
+		m_smoothTime = 0.1f;
 
 		GameObject target = GameObject.Find("Player");
 		if (target != null)
@@ -20,7 +30,7 @@ public class TargetLookAt : MonoBehaviour
         }
     }
 	
-	void Update ()
+	void Update()
 	{
 		UpdatePosition();
     }
@@ -30,10 +40,16 @@ public class TargetLookAt : MonoBehaviour
 		if (m_targetTransform && m_targetPlayer)
 		{
 			int dir = (int)m_targetPlayer.getDirection();
+			m_directionPosition = m_targetTransform.right * dir * m_distanceToTarget; ;
+			Vector3 desiredDirPosition = Vector3.SmoothDamp(transform.position - m_targetTransform.position, m_directionPosition, ref m_currentVelocity, m_smoothTime);
 
-			transform.position = 
-				m_targetTransform.position + 
-				m_targetTransform.right * dir * m_distanceToTarget;
+			transform.position = m_targetTransform.position + desiredDirPosition;
+			transform.localEulerAngles = m_targetTransform.localEulerAngles;
 		}
+	}
+
+	public void SetDistance(float distance)
+	{
+		m_distanceToTarget = distance;
 	}
 }
